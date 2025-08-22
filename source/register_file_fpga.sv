765ca4597fc380e39d3f38c4a99cc2633ec5c11e
ECE 437L <ece437l@ecn.purdue.edu>
2025-08-21 20:24:22 -0400

    rf xilinx update

--- source/register_file_fpga.sv
+++ source/register_file_fpga.sv
@@ -1,6 +1,8 @@
 /*
   Eric Villasenor
   evillase@gmail.com
+  Jimmy Jin
+  mingze.jin01@gmail.com
 
   register file fpga wrapper
 */
@@ -9,25 +11,24 @@
 `include "register_file_if.vh"
 
 module register_file_fpga (
-  input logic CLOCK_50,
-  input logic [3:0] KEY,
-  input logic [17:0] SW,
-  output logic [17:0] LEDR
+  input logic CLK_100MHZ,
+  input logic [3:0] BTN,
+  input logic [15:0] SW,
+  output logic [15:0] LED
 );
 
   // interface
   register_file_if rfif();
   // rf
-  register_file RF(CLOCK_50, KEY[2], rfif);
+  register_file RF(CLK_100MHZ, ~BTN[2], rfif);
 
-assign rfif.wsel = SW[4:0];
-assign rfif.rsel1 = SW[9:5];
-assign rfif.rsel2 = SW[14:10];
-assign rfif.wdat = {29'b0,SW[17:15]};
-
-assign rfif.WEN = ~KEY[3];
-
-assign LEDR[8:5] = rfif.rdat1[3:0];
-assign LEDR[13:10] = rfif.rdat2[3:0];
+  assign rfif.wsel = SW[3:0];
+  assign rfif.rsel1 = SW[7:4];
+  assign rfif.rsel2 = SW[11:8];
+  assign rfif.wdat = {28'b0,SW[15:12]};
+  
+  assign rfif.WEN = BTN[3];
+  assign LED[8:5] = rfif.rdat1[3:0];
+  assign LED[13:10] = rfif.rdat2[3:0];
 
 endmodule
